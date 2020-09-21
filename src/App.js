209596cdbfb311deb1785c98d0a7c4f5d8cc40ff
Bruna <brustalli@icloud.com>
2020-09-21 15:12:58 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        Open-sourced{" "}
-        <a
-          href="https://github.com/brustalli/final-project-react"
-          target="_blank"
-        >
-          code
-        </a>{" "}
-        by{" "}
-        <a href="https://www.linkedin.com/in/bruna-stalliviere-coelho-8022a987/">
-          Bruna Stalliviere
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          Open-sourced{" "}
+          <a
+            href="https://github.com/brustalli/final-project-react"
+            target="_blank"
+          >
+            code
+          </a>{" "}
+          by{" "}
+          <a href="https://www.linkedin.com/in/bruna-stalliviere-coelho-8022a987/">
+            Bruna Stalliviere
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
