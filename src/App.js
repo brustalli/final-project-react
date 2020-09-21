5d1db017f4b54156cff8b18b215901f507e3bc22
Bruna <brustalli@icloud.com>
2020-09-21 14:53:34 +0100

    Added github repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        Open-sourced{" "}
+        <a
+          href="https://github.com/brustalli/final-project-react"
+          target="_blank"
+        >
+          code
+        </a>{" "}
+        by{" "}
+        <a href="https://www.linkedin.com/in/bruna-stalliviere-coelho-8022a987/">
+          Bruna Stalliviere
+        </a>
+      </footer>
+    </div>
+  );
 }
